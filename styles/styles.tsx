457fc45c6234030c61e6a7d94d302a7a1e2c129d
Gordon Chen <gjchen2005@gmail.com>
2025-07-07 22:24:52 -0700

    Added Standard Colors and Suggested Fonts

--- styles/styles.tsx
+++ styles/styles.tsx
@@ -5,7 +5,14 @@ export const colors = {
   white: '#fff',
   offWhite: '#AFAFAF',
   black: '#000',
+  darkBrown: '#e2c19e',
+  lightBrown: '#582e2d',
+  darkGreen: '#3b8439',
+  midGreen: '#54ab41',
+  lightGreen: '#4dbe46',
 };
+
+//Default Font: Open Sans or Alt: Delius Swash Caps
 
 export const commonStyles = StyleSheet.create({
   textInput: {
